1565d47238afceab735b650152db449ed33447c0
francozeta <perufra@hotmail.com>
2025-03-29 20:13:00 -0500

    Update footer component with social media icons and links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin, FaThreads } from 'react-icons/fa6'
+import { MdEmail } from 'react-icons/md'
 
 export default function Footer() {
   return (
@@ -6,20 +9,20 @@ export default function Footer() {
     <div className="container mx-auto max-w-5xl">
       <div className="flex flex-col md:flex-row justify-between items-center">
         <div className="mb-6 md:mb-0">
-          <p className="text-sm text-gray-500">© {new Date().getFullYear()} Franco Zeta. All rights reserved.</p>
+          <p className="text-sm text-neutral-500">© {new Date().getFullYear()} Franco Zeta. All rights reserved.</p>
         </div>
         <div className="flex space-x-6">
-          <a href="#" className="text-gray-500 hover:text-white transition-colors">
-            GitHub
+          <a href="https://www.github.com/francozeta" className="text-neutral-500 hover:text-white transition-colors" target='_blank'>
+            <FaGithub className="w-5 h-5" />
           </a>
-          <a href="#" className="text-gray-500 hover:text-white transition-colors">
-            LinkedIn
+          <a href="#" className="text-neutral-500 hover:text-white transition-colors" target='_blank'>
+          <FaLinkedin className="w-5 h-5" />  
           </a>
-          <a href="#" className="text-gray-500 hover:text-white transition-colors">
-            Twitter
+          <a href="https://www.threads.net/@frxnco.zeta" className="text-neutral-500 hover:text-white transition-colors" target='_blank'>
+          <FaThreads className="w-5 h-5" />
           </a>
-          <a href="#" className="text-gray-500 hover:text-white transition-colors">
-            Contact
+          <a href="#" className="text-neutral-500 hover:text-white transition-colors" target='_blank'>
+            <MdEmail className="w-5 h-5" />
           </a>
         </div>
       </div>
